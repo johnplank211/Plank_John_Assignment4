4eec67ee636a78c21ec4eec4e7b4cd6c8f165a6e
John plank <johnplank211@gmail.com>
2012-04-23 10:21:11 -0400

    first one

--- Plank_John_Assignment4.js
+++ Plank_John_Assignment4.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// John Plank
-//YEAAAA
-=======
-alert("JavaScript works!");
-//plank john 
->>>>>>> c29b60e4f63728a3a842a25c0852ad9247f1db26
+//Plank John
+//Assignment 4
+//Functions
